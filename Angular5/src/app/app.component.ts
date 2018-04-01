74965cd4aab3fd0c212f0e5d3e7056c425cc1ce0
Alex <abrasseur.pro@gmail.com>
2018-04-01 23:41:42 +0200

    Add ng-bootstrap and imports

--- Angular5/src/app/app.component.ts
+++ Angular5/src/app/app.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	template: '<router-outlet></router-outlet>'
 })
-export class AppComponent {
-  title = 'app';
-}
+export class AppComponent { }
--- Angular5/src/app/app.module.ts
+++ Angular5/src/app/app.module.ts
@@ -1,18 +1,36 @@
+// Main modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+// Services
 
 
+// Layout + AppComponent
 import { AppComponent } from './app.component';
 
+// Routes
+const routes: Routes = [	
+	// { path: 'login', 	component: LoginComponent },
+	// { path: 'logout', 	component: LogoutComponent },
+	// { path: 'admin', 	loadChildren: 'app/admin/admin.module#AdminModule', canActivate: [AuthGuard] },
+	// { path: '', 		loadChildren: 'app/pages/pages.module#PagesModule'},
+	// { path: '**', 		redirectTo: 'page-inconnue' }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [		// Components
+		AppComponent
+	],
+	imports: [			// Modules
+		BrowserModule,
+		NgbModule.forRoot(),
+		RouterModule.forRoot(routes)
+	],
+	providers: [		// Services
+	],
+	bootstrap: [ AppComponent ]
 })
 export class AppModule { }
