9ae0cfc2162336e7ec279f298f9b586071740890
Alex <abrasseur.pro@gmail.com>
2018-05-24 01:43:35 +0200

    Get user info

--- Angular5/src/app/pages/account/account.component.ts
+++ Angular5/src/app/pages/account/account.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ICollection } from 'ngx-jsonapi';
+import { UserService } from '../../models/user/user.service';
+import { User } from '../../models/user/user';
 
 @Component({
-  selector: 'app-account',
-  templateUrl: './account.component.html',
-  styles: []
+	selector: 'app-account',
+	templateUrl: './account.component.html'
 })
-export class AccountComponent implements OnInit {
+export class AccountComponent {
+	me: User;
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+	constructor(private userService: UserService) {
+		let id = String(this.userService.user_id)
+		// TODO id
+		this.userService.get(id).subscribe(
+			(user: User) => {
+				console.log(user)
+				this.me = user
+			}
+		)
+	}
 
 }
