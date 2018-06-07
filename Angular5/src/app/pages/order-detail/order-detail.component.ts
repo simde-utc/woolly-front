0283e952de37b0b85757403e78cead6dfe2b4918
Alex <abrasseur.pro@gmail.com>
2018-06-07 02:42:59 +0200

    Refactor multiple fields edition

--- Angular5/src/app/pages/order-detail/order-detail.component.ts
+++ Angular5/src/app/pages/order-detail/order-detail.component.ts
@@ -4,6 +4,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { of } from 'rxjs/observable/of';
 import { catchError, map, tap, finalize } from 'rxjs/operators';
+import { forkJoin } from 'rxjs/observable/forkJoin';
 
 import { JsonApiService } from '../../models/json-api.service';
 import { AuthService } from '../../models/auth.service';
@@ -31,60 +32,50 @@ export class OrderDetailComponent {
 		private router: Router,
 	) {
 		const id = this.route.snapshot.params.id;
+		// Appel callback
 		this.paymentService.checkOrder(id).subscribe(
 			// resp => console.log(resp),
 			// err => console.warn(err),
-			() => this.getOrder(id).subscribe(
-				(order: Order) => {
-					this.order = order;
-					// console.log(order);
-				},
-				err => this.router.navigate['/ventes'],
-				() => this.loading = false
-			)
+			() => this.getOrder(id)
 		);
 	}
 
 	private getOrder(id: string) {
-		// 'orderlines.item.itemfields', 'orderlines.item.itemfields.field',
-		let includes = ['sale', 'orderlines', 'orderlines.item', 'orderlines.orderlineitems', 'orderlines.orderlineitems.orderlinefields',
-			'orderlines.item.itemfields', 'orderlines.orderlineitems.orderlinefields.field']
-		return this.jsonApiService.findRecord(Order, id, { include: includes.join(',') });
+		this.loading = true;
+		let includes = ['sale', 'orderlines', 'orderlines.item', 'orderlines.orderlineitems', 'orderlines.orderlineitems.orderlinefields', 'orderlines.item.itemfields']
+		return this.jsonApiService.findRecord(Order, id, { include: includes.join(',') }).subscribe(
+			(order: Order) => this.order = order,
+			err => this.router.navigate['/ventes'],
+			() => this.loading = false
+		);
 	}
 
-	private hasEditableField(order: Order): boolean {
-		let res = true;
-		order.orderlines.forEach(orderline => {
-			orderline.item.itemfields.forEach(field => {
-				if (field.editable)
-					res = false;
+	private hasNoEditableField(order: Order): boolean {
+		order.orderlines.forEach((orderline: OrderLine) => {
+			orderline.item.itemfields.forEach((itemfield: ItemField) => {
+				if (itemfield.editable)
+					return false;
 			});
 		});
-		return res;
+		return true;
 	}
 
 	private modifyOrderLineFields(order: Order) {
-		let order_save = order;
-		if (this.editmode) {
-			// console.log(orderlineitem.orderline.id);
-			this.jsonApiService.findRecord(Order, order.id, {include: 'orderlines'}).subscribe(
-				(orderstore: Order) => {
-					orderstore.orderlines.forEach(orderline => {
-						for (let i = 0; i < orderline.orderlineitems.length; i++) {
-							for (let j = 0; j < orderline.orderlineitems[i].orderlinefields.length; j++) {
-								orderline.orderlineitems[i].orderlinefields[j].save().subscribe();
-							}
-						}
-					});
-				},
-				err => console.error('Erreur lors de la modification des champs')
-			);
-			this.editmode = false;
-			// this.getOrder(this.route.snapshot.params.id).subscribe();
-			this.order = order_save;
-		} else {
-			this.editmode = true;
-		}
+		this.loading = true;
+		let orderlineList: Observable<OrderLineField>[] = [];
+		this.order.orderlines.forEach((orderline: OrderLine) => {
+			orderline.orderlineitems.forEach((orderlineitem: OrderLineItem) => {
+				orderlineitem.orderlinefields.forEach((orderlinefield: OrderLineField) => {		
+					if (orderlinefield.editable)
+						orderlineList.push(orderlinefield.save());
+				})
+			});
+		});
+		forkJoin(orderlineList).subscribe(
+			(orderlinefields: OrderLineField[]) => console.log(orderlinefields),
+			err => console.warn(err),
+			() => this.loading = false
+		);
 	}
 
 	private mapFieldType(type: string) {
@@ -96,14 +87,5 @@ export class OrderDetailComponent {
 			default:
 				return 'text';
 		}
-	}
-
-	private isEditableField(i: ItemField[], idF: number, idIt: number): boolean {
-		let res: boolean = false;
-		i.forEach(f => {
-			if (f.field.id === idF.toString() && f.item.id === idIt.toString())
-				res = f.editable && this.editmode;
-		});
-		return !res;
 	}
 }
