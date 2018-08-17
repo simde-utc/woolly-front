a059e4d1cbcc15795040224a6ec6dc5d6d82e1b9
Alex <abrasseur.pro@gmail.com>
2018-08-17 18:46:10 +0200

    Remove frontUrl

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -2,5 +2,4 @@ export const environment = {
 	production: false,
 	apiUrl: 'http://woolly.etu-utc.fr',
 	apiDomain: 'woolly.etu-utc.fr',
-	frontUrl: 'http://assos.utc.fr/soireedesfinaux/billetterie/',
 };
