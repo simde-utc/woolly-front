efb9f8030e1a545f79d863405c325cd687cef675
Alex <abrasseur.pro@gmail.com>
2019-03-28 02:25:08 +0100

    Retrieve sales list from API using Redux

---
+++ woolly/src/components/common/SalesList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SalesList extends React.Component {
+	render() {
+		return (
+			<div>
+				SalesList
+			</div>
+		);
+	}
+}
+
+export default SalesList;
--- woolly/src/pages/Home.jsx
+++ woolly/src/pages/Home.jsx
@@ -1,13 +1,46 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import actions from '../redux/actions';
+
+import { List, ListItem, ListItemText } from '@material-ui/core';
+import Loader from '../components/Loader';
+
+const decorator = connect(store => ({
+	sales: store.getData('sales'),
+	fetching: store.isFetching('sales'),
+	fetched: store.isFetched('sales'),
+}))
 
 class Home extends React.Component {
+	componentDidMount() {
+		this.props.dispatch(actions.sales.all())
+	}
+
 	render() {
+		const { sales, fetched, fetching } = this.props;
+		window.p = this.props
 		return (
 			<div className="container">
-				Home
+				<h2>Liste des ventes</h2>
+
+				<Loader loading={!fetched || fetching} text="Récupération des ventes en cours...">
+					<List>
+						{sales.count > 0 ? sales.results.map(sale => (
+							<ListItem key={sale.id}>
+								{/* <ListItemIcon><FolderIcon /></ListItemIcon> */}
+								<ListItemText
+									primary={sale.name}
+									// secondary={sale.association.name}
+								/>
+							</ListItem>
+						)) : (
+							<div>No sales</div>
+						)}
+					</List>
+				</Loader>
 			</div>
 		);
 	}
 }
 
-export default Home;
+export default decorator(Home);
