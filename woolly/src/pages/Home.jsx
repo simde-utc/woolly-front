b08d1de852c2073be2ce622816f493979d697c57
Victor Ronfaut <victor.ronfaut@etu.utc.fr>
2019-04-02 16:24:34 +0200

    ajout d'une page Sale pour obtenir les détails de la vente, et refonte de la page home qui pour l'instant n'affiche que ce qu'il y a dans la page Sales

--- woolly/src/pages/Home.jsx
+++ woolly/src/pages/Home.jsx
@@ -1,39 +1,14 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import actions from '../redux/actions';
 
-import Loader from '../components/Loader';
 import SalesList from '../components/common/SalesList';
-import SaleCard from '../components/common/SaleCard';
+import Sales from './Sales';
 
-const decorator = connect(store => ({
-	sales: store.getData('sales'),
-	pagination: store.getPagination('sales'),
-	fetching: store.isFetching('sales'),
-	fetched: store.isFetched('sales'),
-}))
 class Home extends React.Component {
-	componentDidMount() {
-		this.props.dispatch(actions.sales.all({ include: 'association' }))
-	}
-
 	render() {
-		const { sales, fetched, fetching } = this.props;
 		return (
-			<div className="container">
-				<h2>Ventes en cours</h2>
-
-				<Loader loading={!fetched || fetching} text="Récupération des ventes en cours...">
-					{/* <SalesList sales={this.props.sales} /> */}
-					<div style={{ display: 'flex' }}>
-						{this.props.sales.map(sale => (
-							<SaleCard key={sale.id} sale={sale} />
-						))}
-					</div>
-				</Loader>
-			</div>
+			<Sales/>
 		);
 	}
 }
 
-export default decorator(Home);
+export default Home;
---
+++ woolly/src/pages/Sales.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { connect } from 'react-redux';
+import actions from '../redux/actions';
+
+import Loader from '../components/Loader';
+import SaleCard from '../components/common/SaleCard';
+
+const decorator = connect(store => ({
+	sales: store.getData('sales'),
+	pagination: store.getPagination('sales'),
+	fetching: store.isFetching('sales'),
+	fetched: store.isFetched('sales'),
+}))
+
+class Sales extends React.Component {
+    componentDidMount(){
+        this.props.dispatch(actions.sales.all({ include: 'association' }))
+    }
+
+    render(){
+        const { sales, fetched, fetching } = this.props;
+        return(
+			<div className="container" style={{marginTop: "60px"}}>
+                <h2 style={titleStyle}>Liste des ventes</h2>
+                <Loader loading={!fetched || fetching} text=" Récupération des ventes en cours...">
+                    <div style={{ display: 'flex' }}>
+                        {sales.map(sale => (
+                            <SaleCard key={sale.id} sale={sale} />
+                        ))}
+                    </div>
+                </Loader>
+            </div>
+        )
+    }
+}
+
+const titleStyle = {
+    fontFamily: "roboto, sans-serif",
+    fontWeight: "lighter",
+    fontSize: "2em",
+}
+
+export default decorator(Sales)
