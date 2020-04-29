fc91d9028665cf228e409e4235d0dc3521b06ee4
Alexandre Brasseur <abrasseur@meilleursagents.com>
2020-04-29 21:03:55 +0200

    feat(admin): Remove unused AssoSales

--- src/pages/admin/index.jsx
+++ src/pages/admin/index.jsx
@@ -6,7 +6,6 @@ import { hasManagerRights } from '../../utils';
 import Loader from '../../components/common/Loader';
 import Dashboard from './Dashboard';
 import AssoDashboard from './AssoDashboard';
-import AssoSales from './AssoSales';
 import SaleDetail from './SaleDetail';
 import SaleEditor from './SaleEditor/';
 import Error404 from '../Error404';
@@ -29,7 +28,6 @@ export default function AdminSite(props) {
         <Switch>
             <Route exact path="/admin" component={Dashboard} />
             <Route exact path="/admin/assos/:asso_id" component={AssoDashboard} only="asso_manager" />
-            <Route exact path="/admin/sales" component={AssoSales} />
             <Route exact path="/admin/sales/create" component={SaleEditor} />
             <Route exact path="/admin/sales/:sale_id" component={SaleDetail} />
             <Route exact path="/admin/sales/:sale_id/edit" component={SaleEditor} />
