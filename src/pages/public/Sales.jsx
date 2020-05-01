c0f664829d5152e76f5dc9f289b8b81707a19c7d
Alexandre Brasseur <abrasseur@meilleursagents.com>
2020-05-01 23:15:26 +0200

    feat(hook): Change Home and Sales pages to hooks

--- src/pages/public/Sales.jsx
+++ src/pages/public/Sales.jsx
@@ -1,43 +1,34 @@
 import React from 'react'
-import { connect } from 'react-redux';
-import actions from '../../redux/actions';
-import { withStyles } from '@material-ui/core/styles';
+import { useStoreAPIData } from '../../redux/hooks';
+import { makeStyles } from '@material-ui/core/styles';
 
+import { Container } from '@material-ui/core';
 import Loader from '../../components/common/Loader';
 import SaleCard from '../../components/sales/SaleCard';
 
-const connector = connect(store => ({
-	sales: store.get('sales'),
-}))
-
-class Sales extends React.Component {
-	componentDidMount() {
-		if (!this.props.fetched || this.props.sales.data.length === 1)
-			this.props.dispatch(actions.sales.all({ include: 'association' }));
-	}
-
-	render() {
-		const { classes } = this.props;
-		return (
-			<div className="container">
-				<h1>Liste des ventes</h1>
-
-				<Loader fluid loading={this.props.sales.fetching} text="Récupération des ventes en cours...">
-					<div className={classes.container}>
-						{Object.values(this.props.sales.data || {}).map(sale => <SaleCard key={sale.id} sale={sale} /> )}
-					</div>
-				</Loader>
-			</div>
-		);
-	}
-}
-
-const styles = {
+const useStyles = makeStyles({
 	container: {
 		display: 'flex',
 		flexWrap: 'wrap',
 		// overflowX: 'auto',
 	}
-};
+});
 
-export default connector(withStyles(styles)(Sales));
+export default function Sales(props) {
+	const classes = useStyles();
+	const sales = useStoreAPIData('sales', { queryParams: { include: 'association' }});
+
+	return (
+		<Container>
+			<h1>Liste des ventes</h1>
+
+			<Loader fluid loading={sales === undefined} text="Récupération des ventes en cours...">
+				<div className={classes.container}>
+					{Object.values(sales).map(sale => (
+						<SaleCard key={sale.id} sale={sale} />
+					))}
+				</div>
+			</Loader>
+		</Container>
+	);
+}
--- src/pages/public/index.jsx
+++ src/pages/public/index.jsx
@@ -3,12 +3,10 @@ import React from 'react';
 // import SalesList from '../components/common/SalesList';
 import Sales from './Sales';
 
-class PublicSite extends React.Component {
-	render() {
-		return (
-			<Sales/>
-		);
-	}
-}
 
-export default PublicSite;
+// TODO: Custom home
+export default function PublicSite(props) {
+    return (
+        <Sales />
+    );
+};
