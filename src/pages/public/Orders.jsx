f3a5c55783cd86709cb7109383f808ef9e9e58a1
Alexandre Brasseur <abrasseur@meilleursagents.com>
2020-05-08 18:11:48 +0200

    feat(hook): Change Orders page to hooks

--- src/pages/public/Orders.jsx
+++ src/pages/public/Orders.jsx
@@ -1,59 +1,43 @@
 import React from 'react'
-import { connect } from 'react-redux';
+
+import { useDispatch, useSelector } from 'react-redux';
 import actions from '../../redux/actions';
-import { withStyles } from '@material-ui/core/styles';
+import { Container, Box } from '@material-ui/core';
 
 import Loader from '../../components/common/Loader';
 import OrdersList from '../../components/orders/OrdersList';
 
-const ORDERS_PATH = ['auth', 'orders'];
-const connector = connect(store => ({
-	user: store.getData('auth', {}).user,
-	orders: store.get(ORDERS_PATH),
-}))
 
-class Orders extends React.Component {
-	componentDidMount() {
-		if (!this.props.orders.fetched)
-			this.fetchOrders();
-	}
-
-	componentDidUpdate(prevProps) {
-		if (this.props.user && this.props.user !== prevProps.user)
-			this.fetchOrders();
-	}
-
-	fetchOrders = () => {
-		this.props.dispatch(actions.defineUri(`users/${this.props.user.id}/orders`)
-		                           .definePath(ORDERS_PATH)
-		                           .all({ include: 'sale,orderlines,orderlines__item,orderlines__orderlineitems' }));
-	}
-
-	render() {
-		const { classes } = this.props;
-		return (
-			<div className="container">
-				<h1>Mes commandes</h1>
-
-				<Loader fluid loading={this.props.orders.fetching} text="Récupération des commandes en cours...">
-					<div className={classes.container}>
-						<OrdersList
-							orders={Object.values(this.props.orders.data)}
-							updateOrders={this.fetchOrders}
-						/>
-					</div>
-				</Loader>
-			</div>
-		);
-	}
+function fetchOrders(dispatch, userId) {
+	dispatch(
+		actions.defineUri(`users/${userId}/orders`)
+		       .definePath(['auth', 'orders'])
+		       .all({ include: 'sale,orderlines,orderlines__item,orderlines__orderlineitems' })
+	);
 }
 
-const styles = {
-	container: {
-		display: 'flex',
-		flexWrap: 'wrap',
-		// overflowX: 'auto',
-	}
-};
+export default function Orders(props) {
+	const dispatch = useDispatch();
+	const userId = useSelector(store => store.getAuthUser('id', null));
+	const orders = useSelector(store => store.getAuthRelatedData('orders', undefined));
 
-export default connector(withStyles(styles)(Orders));
+	React.useEffect(() => {
+		if (userId)
+			fetchOrders(dispatch, userId);
+	}, [dispatch, userId]);
+
+	return (
+		<Container>
+			<h1>Mes commandes</h1>
+
+			<Loader fluid loading={orders === undefined} text="Récupération des commandes en cours...">
+				<Box display="flex" flexWrap="wrap">
+					<OrdersList
+						orders={Object.values(orders)}
+						updateOrders={() => fetchOrders(dispatch, userId)}
+					/>
+				</Box>
+			</Loader>
+		</Container>
+	);
+}
