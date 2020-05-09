a936f57813fa187ecc8daa4a1f817288229547a4
Alexandre Brasseur <abrasseur@meilleursagents.com>
2020-05-09 11:08:25 +0200

    feat(hook): Change AccountDetails into function

--- src/components/users/AccountDetails.jsx
+++ src/components/users/AccountDetails.jsx
@@ -1,54 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import { Table, TableBody, TableCell, TableRow } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { TableContainer, Table, TableBody, TableCell, TableRow } from '@material-ui/core';
 import { capitalFirst } from '../../utils';
 
-class AccountDetails extends React.Component{
-	reduceTypes = () => (
-		Object.entries(this.props.user.types || {})
-		      .filter(([key, value]) => value)
-		      .map(([key, value]) => capitalFirst(key))
-		      .join(', ')
-	)
-
-	render() {
-		const { classes, user } = this.props
-		return (
-			<div className={classes.container}>
-				<Table>
-					<TableBody>
-						<TableRow className={classes.topborder}>
-							<TableCell className={classes.label}>Email</TableCell>
-							<TableCell className={classes.value}>{user.email}</TableCell>
-						</TableRow>
-						<TableRow>
-							<TableCell className={classes.label}>Prénom</TableCell>
-							<TableCell className={classes.value}>{user.first_name}</TableCell>
-						</TableRow>
-						<TableRow>
-							<TableCell className={classes.label}>Nom</TableCell>
-							<TableCell className={classes.value}>{user.last_name}</TableCell>
-						</TableRow>
-						<TableRow>
-							<TableCell className={classes.label}>Type</TableCell>
-							<TableCell className={classes.value}>{capitalFirst(user.type)}</TableCell>
-						</TableRow>
-					</TableBody>
-				</Table>
-			</div>
-		);
-	}
-}
-
-AccountDetails.propTypes = {
-	classes : PropTypes.object.isRequired
-}
-
-const styles = {
-	container: {
-		overflowX: 'auto',
-	},
+const useStyles = makeStyles({
 	topborder: {
 		borderTop: '1px solid rgba(224, 224, 224, 1)',
 	},
@@ -59,7 +15,38 @@ const styles = {
 	},
 	value: {
 		fontWeight: 200,
-	}
-};
+	},
+});
 
-export default withStyles(styles)(AccountDetails);
+
+export default function AccountDetails ({ user }) {
+	const classes = useStyles();
+	return (
+		<TableContainer>
+			<Table>
+				<TableBody>
+					<TableRow className={classes.topborder}>
+						<TableCell className={classes.label}>Email</TableCell>
+						<TableCell className={classes.value}>{user.email}</TableCell>
+					</TableRow>
+					<TableRow>
+						<TableCell className={classes.label}>Prénom</TableCell>
+						<TableCell className={classes.value}>{user.first_name}</TableCell>
+					</TableRow>
+					<TableRow>
+						<TableCell className={classes.label}>Nom</TableCell>
+						<TableCell className={classes.value}>{user.last_name}</TableCell>
+					</TableRow>
+					<TableRow>
+						<TableCell className={classes.label}>Type</TableCell>
+						<TableCell className={classes.value}>{capitalFirst(user.type)}</TableCell>
+					</TableRow>
+				</TableBody>
+			</Table>
+		</TableContainer>
+	);
+}
+
+AccountDetails.propTypes = {
+	user: PropTypes.object.isRequired,
+};
