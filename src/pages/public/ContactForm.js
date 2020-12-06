9593397b1f4be63e5bb407248c5994d40d8a6ef5
heymanpi <heymanpi@etu.utc.fr>
2020-12-06 19:22:09 +0100

    Using custom fields and display error/success message to user

--- src/pages/public/ContactForm.js
+++ src/pages/public/ContactForm.js
@@ -1,189 +1,119 @@
-import React, { useState } from 'react'
-import {getButtonColoredVariant} from "../../styles";
-import { Container, Grid} from "@material-ui/core";
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
+import React from 'react'
+import {useDispatch, useSelector} from "react-redux";
+import messagesActions from "redux/actions/messages";
+import {apiAxios} from "utils/api";
+import {useFormStyles} from "utils/styles";
+
+import {Box, Container, Grid, Paper, Button} from "@material-ui/core";
 import CloudUploadIcon from '@material-ui/icons/CloudUpload';
-import {useSelector} from "react-redux";
-import {apiAxios } from "../../redux/actions";
-import Loader from "../../components/common/Loader";
-
-const styles = theme => ({
-    title: {
-        fontSize: '4em',
-        margin: 0,
-    },
-    subtitle: {
-        color: theme.palette.text.secondary,
-        fontWeight: 100,
-    },
-    buttonEmpty: {
-        ...getButtonColoredVariant(theme, "warning", "outlined"),
-        margin: theme.spacing(1),
-    },
-    buttonBuy: {
-        ...getButtonColoredVariant(theme, "success", "contained"),
-        margin: theme.spacing(1),
-    },
-    alert: {
-        textAlign: 'center',
-        color: '#f50057',
-    },
-    root: {
-        '& .MuiTextField-root': {
-            margin: theme.spacing(1),
-            width: '25ch',
-        },
-        flexGrow: 1,
-
-    },
-    formControl: {
-        margin: theme.spacing(1),
-        minWidth: 120,
-    },
-    selectEmpty: {
-        marginTop: theme.spacing(2),
-    },
-
-    paper: {
-        padding: theme.spacing(2),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
-    },
-    column: {
-        display: 'flex',
-        flexDirection: 'column',
-        paddingTop: '0 !important',
-        paddingBottom: '0 !important',
-        minWidth: 220,
-    },
-    controls: {
-        maxWidth: 280,
-    },
-    editor: {
-        // borderWidth: 1,
-        // borderStyle: 'solid',
-        // borderColor: 'transparent',
-        padding: theme.spacing(2),
-    },
-    editing: {
-        borderColor: 'yellow',
-    },
-    textF: {
-        margin: 3,
-        padding: 5
-
-    },
-    button: {
-        margin: theme.spacing(1),
-    },
-});
-
+import Loader from "components/common/Loader";
+import FieldGenerator from 'components/common/FieldGenerator';
 
 
 export default function ContactForm() {
-    const user = useSelector(store => store.getAuthUser());
-    const [reasonIndex, setReasonIndex] = useState(0);
-    const [reason, setReason] = useState("");
-    const [message, setMessage] = useState("");
-    const [loading, setLoading] = useState(0);
 
-    async function handleSubmit(e){
-        e.preventDefault();
-        if(message.length < 0 || reasonIndex === 0) {
-            //TODO : validation
-            console.log("Données invalides")
-            return;
+    const error = {};
+    const initialStore = {choix: 0, reason: null, message: null, loading: false}
+    const [store, changeStore] = React.useState(initialStore);
+    const classes = useFormStyles();
+
+    const onChange = event => {
+        const {name, value} = event.target;
+        if (name === 'choix' && choicePossibilites[value - 1]) {
+            changeStore(prevState => ({...prevState, reason: choicePossibilites[value - 1].label}));
         }
-        const reasonToSend = reason
-        const subject = "[WOOLLY][FeedBack] - " + reason + " - " + user.name;
-        const sender = {name: user.name, id: user.id, email: user.email};
-        const datas = {subject: subject, message: message, sender: sender, reason: reasonToSend};
-        setLoading(1);
-        await apiAxios.post("/feedback", datas).then(() => {
-            setReasonIndex(0);
-            setReason("");
-            setMessage("");
-            setLoading(0);
-        });
+        changeStore(prevState => ({...prevState, [name]: value}));
     }
 
+    const Field = new FieldGenerator(store, error, onChange);
+
+    const choicePossibilites = [
+        {
+            label: 'Création d\'une vente', value: '1'
+        },
+        {
+            label: 'Gestion d\'une vente', value: '2'
+        },
+        {
+            label: 'Commande passée', value: '3'
+        },
+        {
+            label: 'Signaler un bug', value: '4'
+        },
+        {
+            label: 'Autre', value: '5'
+        }
+    ];
+
+    const user = useSelector(store => store.getAuthUser());
+    const dispatch = useDispatch();
+
+    async function handleSubmit() {
+
+        const sender = {name: user.name, id: user.id, email: user.email};
+        const datas = {message: store.message, sender: sender, reason: store.reason};
+        changeStore(prevState => ({...prevState, loading: true}));
+
+        try {
+            await apiAxios.post("/feedback", datas).then(() => {
+                changeStore(initialStore);
+                dispatch(messagesActions.pushMessage("Message envoyé !", "Votre message a bien été transmis au SiMDE", "success"));
+            });
+        } catch (error) {
+            changeStore(prevState => ({...prevState, loading: false}));
+            dispatch(messagesActions.pushError(error, "Un ou plusieurs champs sont manquants"));
+        }
+
+    }
 
     return (
-        loading ? (
-            <Loader text="Envoi du message ..." />
+        store.loading ? (
+            <Loader text="Envoi du message ..."/>
         ) : (
+
+
             <Container>
-                <h1>Contacter l'équipe</h1>
-                <form onSubmit={handleSubmit} noValidate autoComplete="off">
-                    <Grid container spacing={3}
-                          direction="row"
+                <Box clone mb={3} mt={6} textAlign="center">
+                    <h2>Contacter le SiMDE</h2>
+                </Box>
+
+                <Paper className={classes.editor}>
+                    <Grid container spacing={13}
+                          direction="column"
                           justify="center"
                           alignItems="center">
 
-                        <Grid container item xs={12}
-                              direction="row"
-                              justify="center"
-                              alignItems="center"
-                        >
-
-                            <FormControl className={styles.formControl}>
-                                <h4>Veuillez sélectionner un motif</h4>
-                                <Select
-                                    labelId="reason_select"
-                                    id="reason"
-                                    value={reasonIndex}
-                                    onChange={(e, select) => {setReasonIndex(e.target.value); setReason(select.props.children)}}
-                                    required
-                                >
-                                    <MenuItem value={1}>Création d'une vente</MenuItem>
-                                    <MenuItem value={2}>Gestion d'une vente</MenuItem>
-                                    <MenuItem value={3}>Commande passée</MenuItem>
-                                    <MenuItem value={4}>Signaler un bug</MenuItem>
-                                    <MenuItem value={5}>Autre</MenuItem>
-
-                                </Select>
-                            </FormControl>
+                        <Grid className={classes.column}>
+                            <h4>Veuillez sélectionner un motif</h4>
+                            {Field.select('choix', "Choix", choicePossibilites, {required: true})}
                         </Grid>
 
-                        <Grid container item xs={12}
-                              direction="row"
-                              justify="center"
-                              alignItems="center"
-                        >
-                            <FormControl         >
-
-                                <TextField
-                                    id="message_content"
-                                    label="Message"
-                                    placeholder="Dîtes nous ce qu'il se passe !"
-                                    fullWidth
-                                    margin="normal"
-                                    multiline
-                                    rows={10}
-                                    rowsMax={25}
-                                    required={true}
-                                    onChange={(e) => setMessage(e.target.value)}
-                                />
-                            </FormControl>
+                        <Grid className={classes.column} >
+                            <h4>Message</h4>
+                            {Field.text('message', 'Message', {
+                                required: true,
+                                multiline: true,
+                                rows: 6,
+                                fullWidth: true
+                            })}
                         </Grid>
+
+                    </Grid>
+
+                    <Box textAlign="center" mt={4}>
                         <Button
                             variant="contained"
                             color="default"
-                            className={styles.button}
-                            startIcon={<CloudUploadIcon />}
+                            startIcon={<CloudUploadIcon/>}
                             onClick={handleSubmit}
                             type="submit"
                         >
                             Envoyer
                         </Button>
-                    </Grid>
-                </form>
+                    </Box>
+                </Paper>
+
             </Container>
         )
-
     );
 }
